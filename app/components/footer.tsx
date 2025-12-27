dfe38704d66312d67a60a869bfdec2ef45d40796
Portfolio User <portfolio@example.com>
2025-12-27 19:46:40 +0100

    added pictures

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,6 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="border-t mt-auto grid place-items-center py-8 text-muted text-center">
-      <Image src="img/test-img.png" alt="Footer image" />
       <p>© 2025 Leila Zaifie · Portfolio</p>
     </footer>
   );
